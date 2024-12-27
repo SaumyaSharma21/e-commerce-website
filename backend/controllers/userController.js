a0e768d07d6584df6883afc5ca4aba4821c9dd0b
saumya <saumyasharma.bt22ece@pec.edu.in>
2024-12-27 20:19:12 +0530

    chore: Fix typo in userController.js and userRoute.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const createToken = (id) => {
-    return jwt.sign({id},process.env.JWT_SECRET)
+    return jwt.sign({id}, process.env.JWT_SECRET)
 }
 
 //Route for user login
@@ -16,21 +16,21 @@ const loginUser = async(req,res)=> {
 const registerUser = async(req,res)=> {
     try{
 
+
         const { name, email, password } = req.body;
 
         //checking user already exists or not
         const exists = await userModel.findOne({email});
         if(exists) {
-            return res.json({success:false, message:"User already exists"})
-
+            return res.json({success:false, message:"User already exists"});
         }
 
         //validating email format and strong password
         if(!validator.isEmail(email)) {
-            return res.json({success:false, message:"Please enter a valid email"})
+            return res.json({success:false, message:"Please enter a valid email"});
         }
-        if(password.lenght < 8) {
-            return res.json({success:false, message:"Password must be atleast 8 characters long"})
+        if(password.length < 8) {
+            return res.json({success:false, message:"Password must be atleast 8 characters long"});
         }
 
         //hashing password
@@ -41,17 +41,17 @@ const registerUser = async(req,res)=> {
             name,
             email,
             password: hashedPassword
-        })
+        });
 
-        const user = await newUser.save()
+        const user = await newUser.save();
 
-        const token = createToken(user._id)
+        const token = createToken(user._id);
 
-        res.json({success:true, token})
+        res.json({success:true, token});
 
     } catch(error) {
         console.log(error);
-        res.json({success:false, message:error.message})
+        res.json({success:false, message:error.message});
 
     }
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { loginUser, registerUser, adminLogin } from '../controllers/userController.js';
+import userRouter from './userRoute.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.post('/admin', adminLogin);
+
 
 export default userRouter;
