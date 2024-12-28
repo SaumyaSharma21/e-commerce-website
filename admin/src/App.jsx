e1a2f49630656edf532635c7882d249c6b20fa11
saumya <saumyasharma.bt22ece@pec.edu.in>
2024-12-28 23:18:22 +0530

    chore: Update App component to include Sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from './components/NAvbar'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <>
+    <div className='bg-gray-50 min-h-screen'>
+      <>
       <Navbar />
-    </>
+      <hr />
+      <div className='flex-w-full'>
+        <Sidebar />
+      </div>
+      </>
+
+    </div>
   )
 }
 
