e36e94e89b5cd1e874ac3fc84facee931a1949be
saumya <saumyasharma.bt22ece@pec.edu.in>
2024-12-28 17:05:13 +0530

    feat: Add product route for API

---
+++ backend/controllers/productController.js
@@ -0,0 +1,22 @@
+
+// Functio to add a product
+const addProduct = async (req, res) => {
+
+}
+
+//Function to list all products
+const listProducts = async (req, res) => {
+    
+}
+
+//Function to remove a product
+const removeProduct = async (req, res) => {
+    
+}
+
+//Function to get info of a product
+const singleProduct = async (req, res) => {
+    
+}
+
+export { addProduct, listProducts, removeProduct, singleProduct }
