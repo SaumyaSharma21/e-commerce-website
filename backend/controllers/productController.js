b7b84701f2c44f3848c6c356988bddab6fb0a67e
saumya <saumyasharma.bt22ece@pec.edu.in>
2024-12-28 21:23:49 +0530

    chore: Refactor productController.js for better error handling and response consistency

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -47,17 +47,40 @@ const addProduct = async (req, res) => {
 
 //Function to list all products
 const listProducts = async (req, res) => {
-    
+    try {
+        const products = await productModel.find({});
+        res.json({success:true, products})
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 //Function to remove a product
 const removeProduct = async (req, res) => {
-    
+  try {
+
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({success:true, message:"Product removed successfully"})
+  } catch (error) {
+    console.log(error)
+    res.json({success:false, message:error.message})
+  }  
 }
 
 //Function to get info of a product
 const singleProduct = async (req, res) => {
-    
+    try {
+
+        const{ productId } = req.body
+        const product = await productModel.findById(productId);
+        res.json({success:true, product})
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 export { addProduct, listProducts, removeProduct, singleProduct };
