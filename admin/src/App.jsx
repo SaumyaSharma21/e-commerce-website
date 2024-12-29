722a04800dc476b32d8eac5a082d272afa1172ae
saumya <saumyasharma.bt22ece@pec.edu.in>
2024-12-29 18:43:29 +0530

    chore: Update Navbar import and fix styling in App component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/NAvbar'
+import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add'
@@ -28,7 +28,7 @@ const App = () => {
         : <>
           <Navbar setToken  = {setToken}/>
           <hr />
-          <div className='flex-w-full'>
+          <div className='flex w-full'>
             <Sidebar />
             <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
             <Routes>
@@ -43,7 +43,7 @@ const App = () => {
           </div>
           </>
       }
-      
+
 
     </div>
   )
