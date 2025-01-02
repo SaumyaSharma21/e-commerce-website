79dfdd7f031d0cb671a5a834670c7d7ecb3e0172
saumya <saumyasharma.bt22ece@pec.edu.in>
2025-01-02 22:37:18 +0530

    chore: Update Orders page to fetch and display user orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,6 +43,15 @@ const placeOrderRazorpay = async (req,res) => {
 // All Orders data for Admin Panel
 const allOrders = async (req,res) => {
 
+    try {
+        const orders = await orderModel.find({})
+        res.json({success: true, orders})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+        
+    }
 }
 
 // User Order Data for Frontend
@@ -61,7 +70,16 @@ const userOrders = async (req,res) => {
 
 // Update Order Status from Admin Panel
 const updateStatus = async (req,res) => {
-    
+    try {
+        
+        const { orderId, status } = req.body
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({success: true, message: 'Status Updated'})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 
 export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus}
